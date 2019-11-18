481b8d8a5a35f0375b1490cbcb218fe66f3c8298
Alba Gómez Pérez <albagope@gmail.com>
2019-11-18 20:59:42 +0100

    create function to calculate number of items and refactor

--- main.js
+++ main.js
@@ -7,43 +7,79 @@ const total = document.querySelector('.total');
 const buy = document.querySelector('.buy-text');
 const url = "./ingredientsList.json";
 
-window.onload = function() {
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
+/**
+ * Create input change events to handle quantity changes
+ */
+function createQuantityInputEvents() {
+    for (let inputAmount of document.querySelectorAll('.item-amount')) {
+        inputAmount.addEventListener('keyup', updateIngredientPrice);
+    }
+}
 
-            // // recorrer ingredients (map) y pintar en el espacio
-          data.recipe.ingredients.map ((item, index) => {
-             ingredientsContainer.innerHTML +=
-                 `<div class="item-container">
+/**
+ * Create checkbox events to handle click action
+ */
+function createCheckboxEvents() {
+    for (let checkbox of document.querySelectorAll('.item')) {
+        checkbox.addEventListener('click', updateIngredientPrice);
+    }
+}
+
+/**
+ * Create HTML with the structure about an ingredient
+ * @param index # of index in the ingredient list
+ * @param item Object with ingredient info
+ * @returns {string} HTML generated
+ */
+function generateIngredientHTML(index, item) {
+    return `<div class="item-container">
                       <input class="item-amount" type="text" value="1" id="quantity_${index}">   
                       <input class="item" type="checkbox" name="ingredient1" id="checkbox_${index}">${item.product}</input>
                       <div class="item-unitPrice" id="unitPrice_${index}">${item.price} €</div>
                       <div class="item-totalPrice" id="totalPrice_${index}"> 0 €</div>
 
                   </div>`;
+}
 
-              //TODO Crear una estructura HTML con los datos del ingrediente:
-              //crear div que contenga: checkbox(con id), input para la cantidad, nombre, marca y precio unitario,
-              // y al final, un div con el precio de ese producto multiplicado por la cantidad
-         });
-
-
-            for (let inputAmount of document.querySelectorAll('.item-amount')){
-                inputAmount.addEventListener('change', updateIngredientPrice);
-            }
-
-            for (let checkbox of document.querySelectorAll('.item')){
-                checkbox.addEventListener('click', updateIngredientPrice);
-                // checkbox.addEventListener('click', subtotalPrice);
-            }
-
-
+window.onload = function () {
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            data.recipe.ingredients.map((item, index) => {
+                ingredientsContainer.innerHTML += generateIngredientHTML(index, item);
+            });
+            createCheckboxEvents();
+            createQuantityInputEvents();
         });
 };
 
-// function about checkbox checked. ToFixed is used to define number of the decimals
-function updateIngredientPrice(element) {
+/**
+ * Calculate the # of items selected and set the value in DOM
+ * @returns {number} # of items
+ */
+function calculateTotalItems() {
+    const allCheckbox = document.querySelectorAll('.item');
+    const sumItems = document.querySelector('.sumItems');
+    let totalItems = 0;
+    for (let i = 0; i < allCheckbox.length; i++) {
+        if (allCheckbox[i].checked) totalItems++
+    }
+    sumItems.innerHTML = totalItems;
+    return totalItems;
+}
+
+/**
+ * Sum all total price ingredient and paint it in subtotal section
+ */
+function calculateSubtotal() {
+    let result = 0;
+    for (let totalIngredientPrice of document.querySelectorAll('.item-totalPrice')) {
+        result += parseFloat(totalIngredientPrice.innerHTML);
+        subtotal.innerHTML = result;
+    }
+}
+
+function calculateItemPrice(element) {
     let id = (element.currentTarget.id).replace('checkbox_', '');
     id = (id).replace('quantity_', '');
 
@@ -57,15 +93,12 @@ function updateIngredientPrice(element) {
     } else {
         totalPrice.innerHTML = '0 €';
     }
+}
 
-    //function which sum all total price ingredient and paint it in subtotal section
-    let result = 0;
-    for (let totalIngredientPrice of document.querySelectorAll('.item-totalPrice')){
-        result += parseFloat(totalIngredientPrice.innerHTML);
-        subtotal.innerHTML = result;
-    }
-
-    //function which show total price with shipping price
+/**
+ * Show total price with shipping price
+ */
+function calculateTotalPrice() {
     const shippingValue = shipping.innerHTML;
     const subtotalValue = subtotal.innerHTML;
     const shippingSum = parseFloat(subtotalValue) + parseFloat(shippingValue);
@@ -73,25 +106,13 @@ function updateIngredientPrice(element) {
     buy.innerHTML = 'Comprar ingredientes ' + parseFloat(shippingSum);
 }
 
-
-//function which give amount of items that it had been checked
-// function amountItems() {
-//    recorrer todos los checkbox y contar cuantos hay en True. Bucle y luego un lenght?
-//    meterlo en el sumItems
-// }
-
-//function which give subtotalPrice
-// function subtotalPrice(){
-//     console.log('estoy aqui');
-    // const totalIngredientPrice = document.querySelectorAll('.item-totalPrice');
-    // console.log(totalIngredientPrice.innerHTML);
-    // subtotal.innerHTML = totalIngredientPrice.currentTarget.value;
-// }
-
-//function which give total price to buy
-// function totalPrice() {
-//    por innerHtml hay que pintar la cantidad en total (donde ahora hay 45€)
-//    y también añadir con esto += lo mismo al apartado de "comprar ingredientes.
-// }
-
-
+/**
+ *  Update ingredient price based on unit price and quantity if the ingredient is checked
+ *  ToFixed is used to define number of the decimals
+ */
+function updateIngredientPrice(element) {
+    calculateItemPrice(element);
+    calculateTotalItems();
+    calculateSubtotal();
+    calculateTotalPrice();
+}
